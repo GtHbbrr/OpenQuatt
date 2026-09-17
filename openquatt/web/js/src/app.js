ce86ce47150072224b4a4d29ca038b806a415d69
Jeroen <jjansen@gmail.com>
2026-09-17 22:27:49 +0200

    Keep controller favicon inside app asset module

--- openquatt/web/js/src/app.js
+++ openquatt/web/js/src/app.js
@@ -1,8 +1,8 @@
 import "./views/shell.js";
 import { handleChange, handleClick, handleFocusChange, handleInput, handleKeyDown, handlePointerDown, handlePointerMove, handlePointerUp, handleSettingsInteractionEnd, handleSettingsInteractionStart, handleWheel } from "./core/entity-actions.js";
+import { FAVICON_DATA_URL } from "./core/embedded-assets.js";
 import { setEventHandlers } from "./core/event-handlers.js";
 import { boot } from "./core/runtime.js";
-import { FAVICON_DATA_URL } from "virtual:embedded-assets";
 
 function ensureFavicon() {
   if (!document.head) {
